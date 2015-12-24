710f62f21de82af0a9680e908ac9971ebab6f91f
Fabian <fernandez.fabian@outlook.com>
2015-12-24 15:02:12 +0100

    route model binding et affichage des listes/tâches

--- app/Project.php
+++ app/Project.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    //
+    public function tasks()
+    {
+        return $this->hasMany('App\Task'); //relation du model Project: Un à plusieurs
+    }
 }
+
+
